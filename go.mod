a49dd440334b3c5460e54363d1855d80b940ac82
jkimunyi-dev@gmail.com <jkkimunyi@gmail.com>
2024-12-07 04:41:49 +0300

    Implemented REPL on main()

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module arbitrary-precision-calculator
 
 go 1.22.0
+
+require go.starlark.net v0.0.0-20241125201518-c05ff208a98f
+
+require (
+	github.com/chzyer/readline v0.0.0-20180603132655-2972be24d48e // indirect
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+)
--- main.go
+++ main.go
@@ -1,17 +1,78 @@
 package main
 
 import (
-	"arbitrary-precision-calculator/internal"
 	"fmt"
 	"log"
+	"os"
+
+	"arbitrary-precision-calculator/internal"
 )
 
+type REPL struct{}
+
+func (r *REPL) Start() {
+	// Implement REPL logic here
+	fmt.Println("Starting REPL...")
+}
+
 func main() {
-	// Example usage of ArbitraryInt
-	num, err := internal.NewArbitraryInt("12345678901234567890")
-	if err != nil {
-		log.Fatalf("Error creating number: %v", err)
+	// If no arguments, start REPL
+	if len(os.Args) == 1 {
+		repl := &REPL{}
+		repl.Start()
+		return
 	}
 
-	fmt.Println("Large Number:", num)
+	// Rest of your main function remains the same
+	// If arguments are provided, attempt to parse as a calculation
+	if len(os.Args) < 4 {
+		fmt.Println("Usage: calculator [num1] [operation] [num2]")
+		fmt.Println("Or run without arguments to start interactive REPL")
+		os.Exit(1)
+	}
+
+	// Parse first number
+	num1, err := internal.NewArbitraryInt(os.Args[1])
+	if err != nil {
+		log.Fatalf("Invalid first number: %v", err)
+	}
+
+	// Parse operation
+	operation := os.Args[2]
+
+	// Parse second number
+	num2, err := internal.NewArbitraryInt(os.Args[3])
+	if err != nil {
+		log.Fatalf("Invalid second number: %v", err)
+	}
+
+	// Perform calculation based on operation
+	switch operation {
+	case "+":
+		fmt.Println(num1.Add(num2))
+	case "-":
+		fmt.Println(num1.Subtract(num2))
+	case "*":
+		fmt.Println(num1.Multiply(num2))
+	case "/":
+		quotient, remainder, err := num1.Divide(num2)
+		if err != nil {
+			log.Fatalf("Division error: %v", err)
+		}
+		fmt.Printf("Quotient: %v\nRemainder: %v\n", quotient, remainder)
+	case "^":
+		result, err := num1.Pow(num2)
+		if err != nil {
+			log.Fatalf("Exponentiation error: %v", err)
+		}
+		fmt.Println(result)
+	case "%":
+		result, err := num1.Modulo(num2)
+		if err != nil {
+			log.Fatalf("Modulo error: %v", err)
+		}
+		fmt.Println(result)
+	default:
+		log.Fatalf("Unsupported operation: %s", operation)
+	}
 }
