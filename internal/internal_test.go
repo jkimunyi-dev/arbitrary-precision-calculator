faec076aac7c83e87e7045cbd7f2a15c783dee4c
jkimunyi-dev@gmail.com <jkkimunyi@gmail.com>
2024-12-07 03:38:55 +0300

    TestSubtraction(0 implementation

--- internal/internal_test.go
+++ internal/internal_test.go
@@ -89,3 +89,30 @@ func TestAddition(t *testing.T) {
 		})
 	}
 }
+
+func TestSubtraction(t *testing.T) {
+	testCases := []struct {
+		a, b     string
+		expected string
+	}{
+		{"456", "123", "333"},
+		{"123", "456", "-333"},
+		{"1000", "1", "999"},
+		{"0", "0", "0"},
+		{"-123", "456", "-579"},
+		{"123", "-456", "579"},
+		{"-123", "-456", "333"},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.a+" - "+tc.b, func(t *testing.T) {
+			numA, _ := NewArbitraryInt(tc.a)
+			numB, _ := NewArbitraryInt(tc.b)
+
+			result := numA.Subtract(numB)
+			if result.String() != tc.expected {
+				t.Errorf("Expected %s, got %s", tc.expected, result.String())
+			}
+		})
+	}
+}
